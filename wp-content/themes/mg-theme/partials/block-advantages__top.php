2a0b7c8feacf8565bc25e14e45c1534bf922f4bf
Max Globa <onyx18121990@gmail.com>
2023-07-11 23:04:06 +0300

    Update block-advantages__top.php

--- wp-content/themes/mg-theme/partials/block-advantages__top.php
+++ wp-content/themes/mg-theme/partials/block-advantages__top.php
@@ -1,19 +1,15 @@
+<?php if( have_rows('adv-top-list') ): ?>
 <section class="advantages advantages__top">
   <div class="container">
     <div class="advantages__list">
-      <div class="advantages__item">
-        <img src="<?php echo IMG; ?>/adv-01.png" />
-        <h4>Овощи и фрукты <span>долго хранятся - не плесневеют, не гниют, не сушатся</span></h4>
-      </div>
-      <div class="advantages__item">
-        <img src="<?php echo IMG; ?>/adv-02.png" />
-        <h4><span>В погребе приятный запах,</span> а не запах сырости и влаги</h4>
-      </div>
-      <div class="advantages__item">
-        <img src="<?php echo IMG; ?>/adv-03.png" />
-        <h4>На стенках и потолке <span>нет конденсата,</span> за заготовками приятно заходить, ничего не капает</h4>
-      </div>
+      <?php while( have_rows('adv-top-list') ) : the_row(); ?>
+        <div class="advantages__item">
+          <img src="<?php the_sub_field('image'); ?>" />
+          <h4><?php the_sub_field('text'); ?></h4>
+        </div>
+      <?php endwhile; ?>
     </div>
   </div>
   <img class="parallax-obj advantages__pomidor" src="<?php echo IMG; ?>/pomidor.png" />
 </section>
+<?php endif; ?>
